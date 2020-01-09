ba3a466bf7eb993767c290c5a79308bd7c73fbf1
Jakub Senko <senko.jakub1@gmail.com>
2020-01-09 22:12:35 +0100

    add games table

---
+++ database/migrations/2020_01_09_210421_create_table_games.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableGames extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('meno');
+            $table->string('zaner');
+            $table->bigInteger('id_publisher');
+            $table->bigInteger('id_developer');
+            $table->timestamp('datum_vydania');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_games');
+    }
+}
